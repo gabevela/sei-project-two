5dc11cea58e2a26ddb2f841e4f415ff607747bcf
gabe.vela <77593670+gabrielgomezvela@users.noreply.github.com>
2022-05-24 00:47:15 -0400

    adding controllers

--- controllers/formCtrl.js
+++
@@ -1,7 +0,0 @@
-
-
-function(req, res, next) {
-    res.send('respond with a resource');
-  }
-
-  
---
+++ controllers/reportCtrl.js
@@ -0,0 +1,7 @@
+function newReport(req, res, next) {
+    res.send('respond with a resource');
+  };
+  
+  module.exports = {
+    newReport
+  }
