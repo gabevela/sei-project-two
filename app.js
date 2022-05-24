33a03be81e49c11934dd83eade896f68e4628c2b
gabe.vela <77593670+gabrielgomezvela@users.noreply.github.com>
2022-05-24 00:15:58 -0400

    adding new report form

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use('/reports', require('./routes/reports.js'));
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
