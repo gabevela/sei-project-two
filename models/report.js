0602d570407847bd91f01e4a065d24a3e625a047
gabe.vela <77593670+gabrielgomezvela@users.noreply.github.com>
2022-05-23 23:32:06 -0400

    adding model

--- models/report.js
+++ models/report.js
@@ -3,4 +3,14 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const infoSchema = new Schema({
+    reportSubType: String,
+    locationNotes: String,
+});
 
+const reportSchema = new Schema({
+    reportType: String,
+    locationCoordinates: [String],
+    timeAndDate: Date,
+    additionalInfo: [infoSchema] 
+});
