b8c41637ca2f072446d1f26e9a599efcd78cf809
gabe.vela <77593670+gabrielgomezvela@users.noreply.github.com>
2022-05-24 02:10:04 -0400

    adding async function

--- controllers/reportCtrl.js
+++ controllers/reportCtrl.js
@@ -1,17 +1,24 @@
+const reportModel = require("../models/report");
+
 function newReport(req, res, next) {
   res.render('new.ejs');
 };
 
-function create(req, res, next){
-   console.log(req.body);
-   res.send('testing the "create" function');
+async function create(req, res, next) {
+  console.log(req.body);
+  let obj = {
+    reportType: req.body.reportType,
+    locationCoordinates: req.body.locationCoordinates,
+    timeAndDate: req.body.timeAndDate
+  }
+  
+  await ReportModel.create(obj); 
+
+  res.send('testing the "create" function');
 
 };
-
-
 
 module.exports = {
-  newReport, 
+  newReport,
   create
 };
-
